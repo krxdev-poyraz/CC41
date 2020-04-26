85c44c99e39d5b3f2825817e1cd4b511292487fa
KRXDevelops-Poyraz <59502404+krxdev-poyraz@users.noreply.github.com>
2020-04-26 04:35:18 +0300

    Add files via upload

---
+++ CC41/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CC41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int healthHero = 200;
+            int numberVillians = 5;
+            int[] villianHealth = { 54 , 23 , 65 , 87 , 12 };
+
+            Array.Sort(villianHealth);
+
+            int deadVillian = 0;
+            for (int i = 0 ; i < numberVillians ; i++)
+            {
+                healthHero -= villianHealth[i];
+                if(healthHero<=0) break;
+                else deadVillian++;
+            }
+
+            Console.WriteLine(deadVillian);
+            Console.ReadLine();
+        }
+    }
+}
